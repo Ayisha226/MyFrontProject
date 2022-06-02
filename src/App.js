4fc418e7adbe69788915fcd22d39b08152db7489
Ayisha226 <101328643+Ayisha226@users.noreply.github.com>
2022-06-02 15:42:55 +0400

    Front

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import Navbar from './Assets/components/Navbar/Navbar'
+
+
+
+
+function App() {
+  return (
+    <div className>
+    <Navbar/>
+    
+    
+    </div>
+  )
+}
+
+export default App
